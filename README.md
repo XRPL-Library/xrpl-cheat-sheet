198e4f140f3deeae166b78e65392c9fc8bce5231
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-07-23 16:25:40 +0530

    XRPL Cheat Sheet Initialisation

---
+++ README.md
@@ -0,0 +1,22 @@
+#XRP Ledger Cheat Sheet
+
+ A curated list of all the awesome things happening in XRP Ledger.
+
+##Explorers
+
+##Nodes
+
+##Dev Utilities
+
+##Bots
+
+##Hooks
+
+##Sidechains
+
+##Projects
+
+##Dev Community
+
+##Miscellaneous
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const xrplSheetJson = require("./xrpl-cheat-sheet.json");
+
+const main = () => {
+    const nL = `\n\n`;
+    let readme_MD_data = `#XRP Ledger Cheat Sheet${nL} A curated list of all the awesome things happening in XRP Ledger.${nL}`;
+
+    // Adding Data from JSON file
+    Object.keys(xrplSheetJson).map(topic => {
+        readme_MD_data += `##${topic}${nL}`;
+    });
+
+    fs.writeFileSync("README.md", readme_MD_data);
+    console.log(readme_MD_data)
+
+};
+
+
+main();
